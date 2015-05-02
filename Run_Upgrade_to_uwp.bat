0cef34f4a495ad33955bcc138237fdba5ebd78ef
andywigley <andy.wigley@outlook.com>
2015-05-02 15:45:32 +0100

    Corrected bat file

--- Run_Upgrade_to_uwp.bat
+++ Run_Upgrade_to_uwp.bat
@@ -1,4 +1,4 @@
 @ECHO OFF
 SET ThisScriptsDirectory=%~dp0
-SET PowerShellScriptPath=%ThisScriptsDirectory%Upgrade_to_uap.ps1
+SET PowerShellScriptPath=%ThisScriptsDirectory%Upgrade_to_uwp.ps1
 PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& '%PowerShellScriptPath%'";
